e2b15ba1b678ca4b65cf9b143fd81f7ea4016b8d
Rusu Traian Cristian <rusutraiancristian@gmail.com>
2024-09-23 18:11:33 +0300

    latest

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/TelluricAI/ai/packages/go
+
+go 1.18
---
+++ mathlib.go
@@ -0,0 +1,12 @@
+// mathlib.go
+package mylib
+
+// Add sums two integers.
+func Add(a, b int) int {
+    return a + b
+}
+
+// Multiply multiplies two integers.
+func Multiply(a, b int) int {
+    return a * b
+}
